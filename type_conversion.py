d721eb64bbca6eda025cd9a0545183202a3ead3d
AntonyJoji <antonyjoji.antony@gmail.com>
2025-09-25 12:51:17 +0530

    type conversion

---
+++ type_conversion.py
@@ -0,0 +1,50 @@
+# x=10
+# y=2.5
+# print(x+y) ## implicit conversion
+# print(type(x+y))#implicit conversion
+
+# ######################################
+
+# a=5
+# print(a)
+# print(type(a))
+# a=float(a) #explicit conversion
+# print(a)
+# print(type(a))
+
+# ######################################
+
+# a=5.5
+# print(a)
+# print(type(a))
+# a=int(a) #explicit conversion
+# print(a)
+# print(type(a))
+
+
+#######################################
+
+# a=100
+# print(a)
+# print(type(a))
+# a= str(a) 
+# print(a)
+# print(type(a))
+
+# ######################################
+
+ls =[1,2,3,4,5]
+print(ls)
+print(type(ls)) 
+print(set(ls))
+print(tuple(ls))
+
+
+
+ls =[[1,2],[3,4]]
+print (dict(ls))
+
+#######################################
+#string to list
+a ="antony"
+print (list(a))
