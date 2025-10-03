a22922e88f418c1758a5946ddbb83dddcbe23332
AntonyJoji <antonyjoji.antony@gmail.com>
2025-10-03 12:10:17 +0530

    datastructure,question 3

---
+++ data_structure.py
@@ -0,0 +1,14 @@
+# building data structures in python
+# list
+# tuple
+# set
+# dictionary
+
+ls = []
+print(type(ls))
+tuple = ()
+print(type(tuple))
+se = set() #{}
+print(type(se))
+dict = {}
+print(type(dict))
