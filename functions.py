85c23158e983e6c735f3261a6c3dc6fe7c88533d
AntonyJoji <antonyjoji.antony@gmail.com>
2025-10-28 12:00:13 +0530

    functtions

---
+++ functions.py
@@ -0,0 +1,109 @@
+# def cal():
+#     print('hello')
+
+# cal()
+# print("done")
+
+
+
+# def addvals():
+#     a=10
+#     b=21
+#     res=a+b
+#     print(res)
+
+# addvals()
+# addvals()
+
+# def addvals(a,b):
+#     res=a+b
+#     print(res)
+
+# addvals(20,30)
+# addvals(1,3)
+# addvals(30,70)   
+
+# def squr_ls(val):
+#     ls=[]
+#     for i in range(1,val+1):
+#         ls.append(i*i)
+#     print (ls)
+
+    
+
+# squr_ls(100)
+
+def squr_ls(val):
+    ls=[]
+    for i in range(1,val+1):
+        ls.append(i*i)
+    return ls
+
+    
+
+# ls =squr_ls(10)
+# ls = squr_ls(100)
+
+# def find_even(a):
+#     res=[]
+#     for i in a:
+#         if i%2==0:
+#             res.append(i)
+#     return res
+
+
+# result = find_even(ls)
+# print (result)
+
+# def avge():
+#     a=int(input("enter the number:"))
+#     numbers=[]
+    
+#     for i in range(a):
+#         num = float(input("Enter a number: "))
+#         numbers.append(num)
+
+#     average = sum(numbers)/len(numbers)
+#     print (average)
+# avge()
+##########or############
+
+# def avrg(val):
+#     ls=[]
+#     for i in range(val):
+#         num=int(input("enter the number"))
+#         ls.append(num)
+#     return sum(ls)/len(ls)
+
+# val= int (input("enter hte count:-"))
+# result=avrg(val)
+# print (result)
+
+
+
+# def avrg():
+#     val = int (input("enter the count:-"))
+#     sum=0
+#     for i in range(val):
+#         sum+=int(input("enter the value;-"))
+#     return sum/val
+##############################################################
+# result =avrg()
+# print (result)
+    
+
+# name1 = "Devin"
+# name2 = "Abin"
+
+# def callme():
+#     print(f"Hi {name1}, and this is {name2}.")
+
+# def call(val):
+#     Name = "Dainty"
+#     print(f"Hi {Name}, he is {val}.")
+
+# call("Awesome")
+# callme()
+
+
+    
