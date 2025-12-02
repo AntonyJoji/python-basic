d60117d825963c88c7d52e825b6836d8f3fb26b4
AntonyJoji <antonyjoji.antony@gmail.com>
2025-12-02 13:34:38 +0530

    oops.:)

--- oops/introduction_oops.py
+++ oops/introduction_oops.py
@@ -27,21 +27,53 @@
 # print (emp1.first)
 
 
+# class employees:
+#     def __init__(self,first,middle=None,last=None,pay=None): ### constructor automatically called when we create object
+#         self.first=first ## instance variable
+#         self.last=last ## instance variable
+#         self.middle=middle
+#         self.pay=pay
+#         self.email=first+last+"@gmail.com"## instance variable
+#     def fullname(self):## 'self' is used to access instance variable and method inside the class
+#            if self.middle is None:
+#                 return self.first+" "+self.last
+#            else:
+#                 return self.first+" "+self.middle+" "+self.last
+           
+# emp1 = employees(first="david",last="mathew",pay=1000)# creating object and passing values to constructor
+#  ## or when we create object constructor automatically called
+# emp2 = employees(first="dainty",last="mathew",pay=4000)
+# emp3 = employees(first="dan",middle='r',last='kumar',pay=5000)
+# # print(emp1.email)
+# # print(emp2.email)
+# # print(emp1.last)
+
+# print(emp1.fullname())
+# print(emp2.fullname())
+# print(emp3.fullname())
+
+
 class employees:
-    def __init__(self,first,last,pay): ### constructor automatically called when we create object
-        self.first=first ## instance variable
-        self.last=last ## instance variable
-        self.pay=pay
-        self.email=first+last+"@gmail.com"## instance variable
-        
+    def __init__(self, first, last, pay, middle=None):
+        self.first = first
+        self.last = last
+        self.middle = middle
+        self.pay = pay
+        self.email = first + last + "@gmail.com"
 
-emp1 = employees("david","mathew",1000)# creating object and passing values to constructor
- ## or when we create object constructor automatically called
-emp2 = employees("dainty","mathew",4000)
-
-print(emp1.email)
-print(emp2.email)
-print(emp1.last)
+    def fullname(self):
+        if self.middle is None:
+            return self.first + " " + self.last
+        else:
+            return self.first + " " + self.middle + " " + self.last
 
 
+first_name=input("enter the first name:")
+middle_name=input ("enter the middle name (press enter if we need to skip):")
+last_name=input("enter the last name:")
+pay=int(input("enter the pay:"))
 
+emp = employees(first=first_name,middle=middle_name,last=last_name,pay=pay)
+print(emp.fullname())
+print (emp.pay)
+print(emp.email)
