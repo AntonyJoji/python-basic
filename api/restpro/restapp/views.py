a38d172b742defc38482e4228a5319282f3ad0a6
AntonyJoji <antonyjoji.antony@gmail.com>
2026-02-06 00:46:29 +0530

    get and post

--- api/restpro/restapp/views.py
+++ api/restpro/restapp/views.py
@@ -10,3 +10,12 @@ class userprofile(APIView):
         profile = user.objects.all()
         newdata =userserializer(profile,many=True)
         return Response(newdata.data)
+    
+
+    def post (self,req):
+        newdata = userserializer(data=req.data)
+        if newdata.is_valid():
+            newdata.save()
+            return Response(newdata.data)
+        else:
+            return Response("data is not valid")
